b669271b83f1501957bd5a091fd13ec28ec55860
treylitefm <griffin.omar@gmail.com>
2016-03-01 00:45:15 -0500

    Error still stands. However, plot initializes on input values instead of starting a few iterations after.
    Signed-off-by: treylitefm <griffin.omar@gmail.com>

--- conway-animation.py
+++ conway-animation.py
@@ -12,25 +12,31 @@ def main():
     numframes = 100
 
     fig = plt.figure()
-    scat = plt.scatter([], [], s=100)
-
     
     grid = init_grid(20); populate(grid, [(10, 10), (10, 13), (11, 10), (11, 11), (12, 10), (12, 12), (13, 13)])
+
+    #def init():
     points = live_cells(grid)
-    x1,y1 = zip(*points)
+    y1,x1 = zip(*points)
     c1 = np.random.random_integers(0, 10, len(points))
+    scat = plt.scatter(x1, y1, c=c1, s=100)
     print list(x1)
     print list(y1)
     print list(c1)
-    print '-'*20
+    print '0'*20
+    #return scat,
+        
 
 
 
+    #anim = animation.FuncAnimation(fig, update_plot, init_func=init, frames=xrange(numframes), fargs=(scat, grid), blit=False)
     anim = animation.FuncAnimation(fig, update_plot, frames=xrange(numframes), fargs=(scat, grid), blit=False)
 
     #anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
     plt.axis([0,20,0,20])
     plt.show()
+
+
 
 
 
@@ -48,10 +54,12 @@ def update_plot(i, scat, grid):
     print '*'*20
     print '-'*20
     '''
-    #import game_of_life; grid = game_of_life.init_grid(10); game_of_life.populate(grid, [(1,1), (2,2), (3,3), (4,4), (1,0), (2,4), (5,5), (2,7), (7,5), (4,5)])
+    points = live_cells(grid) if i == 0 else iterate_grid(grid)
     points = iterate_grid(grid)
-    x1,y1 = zip(*points)
+    y1,x1 = zip(*points)
     c1 = np.random.random_integers(0, 10, len(points))
+    
+    print 'i',i
     print list(x1)
     print list(y1)
     print list(c1)
