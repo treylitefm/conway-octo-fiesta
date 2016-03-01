bfa4715be89c80824eac561482d00694504b87ad
treylitefm <griffin.omar@gmail.com>
2016-02-29 23:50:23 -0500

    Conway initialization on scatterplot.
    Signed-off-by: treylitefm <griffin.omar@gmail.com>

---
+++ pyplot-sample3.py
@@ -0,0 +1,50 @@
+import time
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation
+
+def main():
+    numframes = 100
+
+    fig = plt.figure()
+    scat = plt.scatter([], [], s=100)
+
+    anim = animation.FuncAnimation(fig, update_plot, frames=xrange(numframes), fargs=(scat,), blit=False)
+
+    #anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+    plt.axis([0,40,0,40])
+    plt.show()
+
+
+
+def update_plot(i, scat):
+    plt.cla()
+    time.sleep(1)
+    '''numframes = 100
+    numpoints = 10
+    #x, y, c = np.random.random((3, numpoints))
+    x, y, c = np.random.random_integers(0,40,(3, numpoints))
+    print '*'*20
+    print 'x',x
+    print 'y',y
+    print 'c',c
+    print '*'*20
+    print '-'*20
+    '''
+    #import game_of_life; grid = game_of_life.init_grid(10); game_of_life.populate(grid, [(1,1), (2,2), (3,3), (4,4), (1,0), (2,4), (5,5), (2,7), (7,5), (4,5)])
+    
+    import game_of_life; grid = game_of_life.init_grid(20); game_of_life.populate(grid, [(10, 10), (10, 13), (11, 10), (11, 11), (12, 10), (12, 12), (13, 13)])
+    points = game_of_life.live_cells(grid)
+    x1,y1 = zip(*points)
+    c1 = np.random.random_integers(0, 10, len(points))
+    print list(x1)
+    print list(y1)
+    print list(c1)
+    print '-'*20
+    #import sys; sys.exit()
+
+    scat = plt.scatter(x1, y1, c=c1, s=100)
+    plt.axis([0,20,0,20])
+    return scat,
+
+main()
