a8d25bafe40237aaba4222d71b110c27b3ab288f
treylitefm <griffin.omar@gmail.com>
2016-03-03 21:06:57 -0500

    Remove second call of iterate grid

--- conway-animation.py
+++ conway-animation.py
@@ -9,13 +9,12 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 
 def main():
-    numframes = 100
+    #numframes = 100
 
     fig = plt.figure()
     
     grid = init_grid(20); populate(grid, [(10, 10), (10, 13), (11, 10), (11, 11), (12, 10), (12, 12), (13, 13)])
 
-    #def init():
     points = live_cells(grid)
     y1,x1 = zip(*points)
     c1 = np.random.random_integers(0, 10, len(points))
@@ -24,39 +23,19 @@ def main():
     print list(y1)
     print list(c1)
     print '0'*20
-    #return scat,
-        
-
-
-
-    #anim = animation.FuncAnimation(fig, update_plot, init_func=init, frames=xrange(numframes), fargs=(scat, grid), blit=False)
-    anim = animation.FuncAnimation(fig, update_plot, frames=xrange(numframes), fargs=(scat, grid), blit=False)
+    anim = animation.FuncAnimation(fig, update_plot, interval=1500, fargs=(scat, grid), blit=False)
 
     #anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
     plt.axis([0,20,0,20])
     plt.show()
 
 
-
-
-
 def update_plot(i, scat, grid):
     plt.cla()
-    time.sleep(1)
-    '''numframes = 100
-    numpoints = 10
-    #x, y, c = np.random.random((3, numpoints))
-    x, y, c = np.random.random_integers(0,40,(3, numpoints))
-    print '*'*20
-    print 'x',x
-    print 'y',y
-    print 'c',c
-    print '*'*20
-    print '-'*20
-    '''
+
     points = live_cells(grid) if i == 0 else iterate_grid(grid)
-    points = iterate_grid(grid)
-    y1,x1 = zip(*points)
+
+    y1,x1 = zip(*points) if points != [] else ((),())
     c1 = np.random.random_integers(0, 10, len(points))
     
     print 'i',i
@@ -69,4 +48,6 @@ def update_plot(i, scat, grid):
     plt.axis([0,20,0,20])
     return scat,
 
-main()
+
+if __name__ == "__main__":
+    main()
